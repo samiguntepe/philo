130aed2fb98f7596ebef1efa2ae6f8ddeadb46b2
Sami Güntepe <sguntepe@student.42kocaeli.com.tr>
2023-09-27 19:50:43 +0300

    42

--- dinner_utils.c
+++ dinner_utils.c
@@ -6,7 +6,7 @@
 /*   By: sguntepe <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 15:01:55 by sguntepe          #+#    #+#             */
-/*   Updated: 2023/09/27 19:40:58 by sguntepe         ###   ########.fr       */
+/*   Updated: 2023/09/27 19:50:30 by sguntepe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@ void	eat(t_philo *philos, int phid)
 	(void)phid;
 	pthread_mutex_lock(&philos->args->forks[0]);
 	pthread_mutex_lock(&philos->args->forks[1]);
+	pthread_mutex_lock(&philos->args->forks[2]);
 	write_term(philos->id, 1, philos->args);
 	write_term(philos->id, 1, philos->args);
 	write_term(philos->id, 2, philos->args);
 	usleep(philos->args->time_to_eat * 1000);
+
+	
 	pthread_mutex_unlock(&philos->args->forks[0]);
 	pthread_mutex_unlock(&philos->args->forks[1]);
+	pthread_mutex_unlock(&philos->args->forks[2]);
 }

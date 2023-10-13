dab9aaab40ae2cb7167c5dd661987a41c4d6c1ef
Sami Güntepe <sguntepe@student.42kocaeli.com.tr>
2023-10-13 13:44:09 +0300

    42

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sguntepe <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 14:27:16 by sguntepe          #+#    #+#             */
-/*   Updated: 2023/10/13 13:05:19 by sguntepe         ###   ########.fr       */
+/*   Updated: 2023/10/13 13:41:26 by sguntepe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ int	main(int argc, char **argv)
 
 	args.died = 0;
 	if (argc < 5 || argc > 6)
-		return (0);
+		return (write(2, RED"Error\n"END, 13));
 	if (control(argv))
-		return (0);
+		return (write(2, RED"Error\n"END, 13));
 	args.first_time = get_time();
 	philos = malloc(sizeof(t_philo) * ft_atoi(argv[1]));
 	if (!philos)
-		return (1);
+		return (write(2, RED"Error\n"END, 13));
 	arg_parser(argv, &args, argc);
 	if (arg_control(&args, argc) == 0)
 	{
@@ -36,6 +36,6 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	if (inits(&args, philos))
-		return (1);
+		return (write(2, RED"Error\n"END, 13));
 	free_memory(philos, 2);
 }

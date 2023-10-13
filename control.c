76befe3adb6ba46ee2d0219257cb3b737ffff3f8
Sami Güntepe <sguntepe@student.42kocaeli.com.tr>
2023-10-13 13:35:44 +0300

    42

--- control.c
+++ control.c
@@ -6,7 +6,7 @@
 /*   By: sguntepe <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 13:11:32 by sguntepe          #+#    #+#             */
-/*   Updated: 2023/10/11 15:12:31 by sguntepe         ###   ########.fr       */
+/*   Updated: 2023/10/13 13:32:53 by sguntepe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,25 @@ int	arg_control(t_arg *args, int argc)
 int	control(char **argv)
 {
 	int	i;
+	int	j;
 
-	i = 0;
+	i = 1;
 	while (argv[i])
 	{
 		if (argv[i][0] == '-')
 		{
 			write(2, RED"Error\n"END, 13);
 			return (1);
+		}
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+				write(2, RED"Error\n"END, 13);
+				return (1);
+			}
+			j++;
 		}
 		i++;
 	}
